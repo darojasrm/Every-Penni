6c85c2189a080fed7206d6518f898223ba491312
Daniel Rojas <51244101+darojasrm@users.noreply.github.com>
2026-05-13 14:46:34 -0400

    Fix CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,13 @@ const allowedOrigins = [
 // MIDDLEWARE
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
